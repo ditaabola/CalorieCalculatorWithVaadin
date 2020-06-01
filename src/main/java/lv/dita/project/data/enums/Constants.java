6238f219041fc266d1d0e27567a228683288d624
gundegaA <60237828+gundegaA@users.noreply.github.com>
2020-06-01 21:07:43 +0300

    ceru, ka bus tagad visas izmainas

--- src/main/java/lv/dita/project/data/enums/Constants.java
+++ src/main/java/lv/dita/project/data/enums/Constants.java
@@ -1,4 +1,4 @@
-package lv.dita.project.data;
+package lv.dita.project.data.enums;
 
 public class Constants {
     public static final String SCHEMA_NAME = "calories_all"+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
