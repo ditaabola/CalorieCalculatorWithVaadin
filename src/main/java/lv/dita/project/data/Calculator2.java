0f7eafae712fbf808d728289741db6448a0aedda
gundegaA <60237828+gundegaA@users.noreply.github.com>
2020-06-03 16:09:44 +0300

    Upgraded BMI calculation in UI

--- src/main/java/lv/dita/project/data/Calculator2.java
+++ src/main/java/lv/dita/project/data/Calculator2.java
@@ -1,5 +1,8 @@
 package lv.dita.project.data;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.text.DecimalFormat;
 
 public class Calculator2 {
@@ -9,7 +12,7 @@ public class Calculator2 {
 
     public static String calculateBMI(double weight, double height) {
 
-        if (weight <= 0 || height <= 0 ) {
+        if (weight <= 0 || height <= 0) {
             return "Invalid data!";
 
         } else {
@@ -19,5 +22,24 @@ public class Calculator2 {
             return df.format(bmi);
         }
     }
+
+    @Contract(pure = true)
+    public static @NotNull String commentAboutUsersBmi(double weight, double height) {
+
+        double bmi = weight / ((height / 100) * (height / 100));
+        if (bmi < 18.5) {
+            return "You're in the underweight range.";
+        } else if (bmi > 18.5 && bmi <= 24.9) {
+            return "You're in the healthy weight range.";
+        } else if (bmi > 25 && bmi <= 29.9) {
+            return "You're in the overweight range.";
+        } else if (bmi > 30 && bmi <= 39.9) {
+            return "You're in the obese range.";
+        } else if (bmi > 39.9) {
+            return "Your health is in danger. Please consult your doctor ASAP!";
+        }
+        return "Something went wrong.";
+    }
+
 }
 
