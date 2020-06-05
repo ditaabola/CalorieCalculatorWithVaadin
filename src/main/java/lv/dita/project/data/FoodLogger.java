4b90d1be974e39e5592109bbb959893b951ae717
ditaabola <dita.abola@gmail.com>
2020-06-06 00:47:48 +0300

    FoodLayout changed, FoodCategory added, FoodLogger class added

--- src/main/java/lv/dita/project/data/FoodLogger.java
+++ src/main/java/lv/dita/project/data/FoodLogger.java
@@ -1,6 +1,26 @@
 package lv.dita.project.data;
 
+import lv.dita.project.data.interfaces.DataRepository;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodLogger {
 
+    private Food food;
+    private DataRepository repo = new MySqlDataRepository();
 
+
+
+    public String calculateCalories(String food, Double quantity, Double caloriesPer100G) {
+        double caloriesEaten = 0;
+        List<String> items = new ArrayList<>();
+        for (String item : items) {
+            double caloriesPerFoodEaten = (caloriesPer100G / 100) * quantity;
+            caloriesEaten += caloriesPerFoodEaten;
+        }
+        DecimalFormat df = new DecimalFormat("##.##");
+        return df.format(caloriesEaten);
+    }
 }
